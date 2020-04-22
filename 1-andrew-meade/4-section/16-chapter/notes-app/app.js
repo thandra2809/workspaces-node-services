ef06ee9517fc0375f1bdc4099be65c7b8fff80d5
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-04-22 02:23:54 -0700

    Initial changes

--- 1-andrew-meade/4-section/16-chapter/notes-app/app.js
+++ 1-andrew-meade/4-section/16-chapter/notes-app/app.js
@@ -9,8 +9,16 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function () {
-        console.log('Adding a new note!')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+
+    },
+    handler: function (argv) {
+        console.log('Adding a new note!', argv)
     }
 })
 
