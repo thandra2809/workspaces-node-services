20bb18b59d2bd121023f1b73064a6d970f298a77
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-04-22 22:23:04 -0700

    Initial changes

--- 1-andrew-meade/4-section/21-chapter/play-ground/2-arrow-function.js
+++ 1-andrew-meade/4-section/21-chapter/play-ground/2-arrow-function.js
@@ -3,3 +3,17 @@ const square = (x) => {
 }
 
 console.log(square(3))
+
+const event = {
+    name: 'Birthday Party',
+    guestList: ['Andrew', 'Jen', 'Mike'],
+    printGuestList() {
+        console.log('Guest list for ' + this.name)
+
+        this.guestList.forEach((guest) => {
+            console.log(guest + ' is attending ' + this.name)
+        })
+    }
+}
+
+event.printGuestList()
